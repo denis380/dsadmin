f4f1b310dd504638a01707a8ed2ca307bc29d836
Denis dos Reis <denis380@gmail.com>
2019-05-07 22:53:48 -0300

    .

--- impressora/models.py
+++ impressora/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django import forms
 
 class Printer(models.Model):
+    serial = models.CharField(max_length=20)
     ip = models.GenericIPAddressField()
-    serial = models.CharField(max_length=20, primary_key=True)
     modelo = models.CharField(max_length=8)
     contador = models.IntegerField()
     etiqueta = models.CharField(max_length=15)
